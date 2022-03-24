043cfe874d5988a8d5c825fee3f1f27dec0179ee
MrDoCa <80938620+MrDoCa@users.noreply.github.com>
2022-03-24 10:50:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -84,7 +84,7 @@ async def start(client, message):
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
-            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=Script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             video="https://telegra.ph/file/fc3b31cdbfbe9fcf16cd9.mp4",
             reply_markup=reply_markup
         ) 
